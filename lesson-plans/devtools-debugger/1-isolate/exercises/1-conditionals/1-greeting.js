dfeb2ccc6b11b679be42256df9f6e8f9c2317d1a
Milena Wasowicz <milena.wasowicz@gmail.com>
2023-05-24 23:35:47 +0200

    do the exercises

--- lesson-plans/devtools-debugger/1-isolate/exercises/1-conditionals/1-greeting.js
+++ lesson-plans/devtools-debugger/1-isolate/exercises/1-conditionals/1-greeting.js
@@ -1,12 +1,13 @@
 'use strict';
 
 let input = prompt('enter your name:');
+let input2 = prompt('enter your last name:');
 
 let greeting = '';
 if (input === null) {
   greeting = 'hello anonymous.';
 } else {
-  greeting = 'hello ' + input + '!';
+  greeting = 'hello ' + input + ' ' + input2 + '!';
 }
 
 alert(greeting);
